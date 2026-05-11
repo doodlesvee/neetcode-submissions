f851c7c648573986acb702bdd28be0d3068c018c
Vivek Jaiswal <vivek29jaiswals@gmail.com>
2026-05-11 21:12:35 +0530

    Add: minimum-stack - submission-1

---
+++ Data Structures & Algorithms/minimum-stack/submission-1.js
@@ -0,0 +1,61 @@
+class MinStack {
+    constructor() {
+    this.stack = [];
+    this.minStack = [];
+  }
+
+  push(val) {
+    if (!this.minStack.length || this.minStack.at(-1) >= val)
+      this.minStack.push(val);
+    this.stack.push(val);class MinStack {
+  constructor() {
+    this.stack = [];
+    this.minStack = [];
+  }
+
+  push(val) {
+    this.stack.push(val);
+
+    if (
+      this.minStack.length === 0 ||
+      val <= this.minStack[this.minStack.length - 1]
+    ) {
+      this.minStack.push(val);
+    }
+  }
+
+  pop() {
+    const val = this.stack.pop();
+
+    if (val === this.minStack[this.minStack.length - 1]) {
+      this.minStack.pop();
+    }
+
+    return val;
+  }
+
+  top() {
+    return this.stack[this.stack.length - 1];
+  }
+
+  getMin() {
+    return this.minStack[this.minStack.length - 1];
+  }
+}
+  }
+
+  pop() {
+    let val = this.stack.pop();
+    if (val === this.minStack.at(-1)) this.minStack.pop();
+
+    return val
+  }
+
+  getMin() {
+    return this.minStack.at(-1);
+  }
+
+  top() {
+    return this.stack.at(-1);
+  }
+}

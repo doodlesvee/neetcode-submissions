603674cf2bfe57ff42357f00695001b81e5ef4a9
Vivek Jaiswal <vivek29jaiswals@gmail.com>
2026-06-09 22:27:04 +0530

    Add: duplicate-integer - submission-1

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    hasDuplicate(nums) {
+          const obj = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    if (!obj.hasOwnProperty(nums[i])) {
+      obj[nums[i]] = 1;
+    } else {
+      return true;
+    }
+  }
+
+  return false
+    }
+}
